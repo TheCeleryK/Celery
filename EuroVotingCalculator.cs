2f4666fd9e165fb6057cf3b1bea519fbb2aa9e16
TheCeleryK <61520699+TheCeleryK@users.noreply.github.com>
2020-02-27 02:41:37 +0000

    Add files via upload

--- EuroVotingCalculator.cs
+++ EuroVotingCalculator.cs
@@ -2,28 +2,29 @@
 
 namespace European_Council_Voting_Calculator
 {
+    //All is encapsulated within this Class
     class Calculator
     {
         public static void Main(string[] args)
         {
-
+            //Called upon later in the code.
             int voteYes;
             float Population;
-            int Check1 = 0;
-            int Check2 = 0;
+            int voteCheck = 0;
+            int popCheck = 0;
 
-
+            //Data is adstracted from country names to numbers.
             Console.Write("How many countries voted For? ");
             voteYes = int.Parse(Console.ReadLine());
-            if (voteYes > 15)
-            { Check1 = Check1 + 1; }
+            if (voteYes > 14)
+            { voteCheck = voteCheck + 1; }
 
             Console.Write("What percentage of the population? ");
             Population = int.Parse(Console.ReadLine());
             if (Population > 64.99)
-            { Check2 = Check2 + 1; }
+            { popCheck = popCheck + 1; }
 
-            if (Check1 + Check2 == 2)
+            if (voteCheck + popCheck == 2)
             { Console.WriteLine("Passed!"); }
             else
                 Console.WriteLine("Denied!"); 
